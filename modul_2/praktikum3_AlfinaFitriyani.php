084807c76859e5d121e38891939f2e1c688804b3
Alfina Fitriyani <alfinayani931@gmail.com>
2023-10-09 18:52:03 +0700

    modul

---
+++ modul_2/praktikum3_AlfinaFitriyani.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Modul 2 - Tugas Praktikum 3</title>
+    </head>
+    <body>
+        <center>
+            <h1>SELAMAT DATANG DI WEBSITE RPL POLIBANG JEPARA </h1>
+            <hr>
+            <br>
+</center>
+            <h2>Ayo Kita Belajar Web Developer Bersama!</h2>
+            <p>Nama Mahasiswa : Alfina Fitriyani</p>
+            <p>Alamat : Jebol RT 06/RW 01, Kec.Mayong Kab.Jepara</p>
+    <p>Ayo Kujungi Website Polibang! <a href="https://www.polibang.ac.id"> "Polibang Jepara"</a></p>
+    <p>Kalian Juga Dapat Belajar Pemograman Web di  <a href="https://www.w3school.com">"w3school"</a></p>
+    <p>Selamat Belajar!</p>
+</body>
+</html>
+ 
