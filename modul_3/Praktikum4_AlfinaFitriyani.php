69de3df3126c34c89ad4b982fc77bcaaf146ca1e
Alfina Fitriyani <alfinayani931@gmail.com>
2023-10-16 19:14:18 +0700

    modul 3

---
+++ modul_3/Praktikum4_AlfinaFitriyani.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Modul 3 - Praktikum 4</title>
+</head>
+
+<frameset cols="20%, 80%">
+    <frame src="list_nama.html" name="nama">
+    <frameset rows="20%, 80%">
+        <frame src="header.html" name="header" > 
+            <frame src="mahasiswa.html" name="mahasiswa_detail" >
+        </frameset>
+    </frameset>
+</frameset>
+</html>
